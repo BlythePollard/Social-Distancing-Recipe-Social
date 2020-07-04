6be3422a2a03c055e68de059c6da4b7ef7968fe0
Blythe Pollard <blytheepollard@gmail.com>
2020-07-03 19:23:57 -0600

    added saved recipe index and show page,but no image

--- app/controllers/SavedRecipesController.rb
+++ app/controllers/SavedRecipesController.rb
@@ -2,13 +2,17 @@ class SavedRecipesController < ApplicationController
 
     def new
         @recipe = Recipe.find_by(id: params[:id])
-        SavedRecipe.create(title: @recipe.title, description: @recipe.description, link: @recipe.link, user_id: @recipe.user_id)
+        @saved_recipe = SavedRecipe.create(title: @recipe.title, description: @recipe.description, link: @recipe.link, user_id: @recipe.user_id)
         flash[:notice] = "Recipe Saved!"
         redirect_to group_path(@recipe.group_id)
     end
 
     def index
         @saved_recipes = SavedRecipe.all
+    end
+
+    def show
+        @saved_recipe = SavedRecipe.find_by(id: params[:id])
     end
 
 end
